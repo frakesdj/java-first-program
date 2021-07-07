6e285ff3228b8d0dceab450da4193bf5b152869b
frakesdj <54415030+frakesdj@users.noreply.github.com>
2021-07-07 16:22:13 -0400

    Add files via upload

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,19 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
     }
+    
+    private static int add(int[] numbers)
+    {
+    	int sum = 0;
+    	for (int x = 0; x < numbers.length; x++)
+    	{
+    		sum = sum + numbers[x];
+    	}
+    	return sum;
+    }
+    
+
+    
 }
