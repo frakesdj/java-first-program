e56f480c1d56eb7fbe88a12e7ed1976fbb998a6e
frakesdj <54415030+frakesdj@users.noreply.github.com>
2021-07-07 16:21:30 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int doubleTheNumber(int number) {
+        return number * 2;
+    }
+    
+    private static int add(int[] numbers)
+    {
+    	int sum = 0;
+    	for (int x = 0; x < numbers.length; x++)
+    	{
+    		sum = sum + numbers[x];
+    	}
+    	return sum;
+    }
+    
+
+    
+}
